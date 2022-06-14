4d8a9ec9af733c828cd264f78134a4cc84069fff
Peter McEvoy <peter@tlon.io>
2022-06-14 16:47:15 -0700

    http-client: confine response sending to single location

--- src/http/client.rs
+++ src/http/client.rs
@@ -29,6 +29,12 @@ pub enum Error {
     Hyper(HyperError),
 }
 
+impl Response for Error {
+    fn serialize(self) -> Vec<u8> {
+        todo!()
+    }
+}
+
 impl From<HyperError> for Error {
     fn from(err: HyperError) -> Self {
         Self::Hyper(err)
@@ -36,11 +42,7 @@ impl From<HyperError> for Error {
 }
 
 /// Send an HTTP request and receive its response.
-async fn send_request(
-    client: Client<HttpConnector>,
-    req: Vec<u8>,
-    resp_tx: Sender<Vec<u8>>,
-) -> Result<(), Error> {
+async fn send_request(client: Client<HttpConnector>, req: Vec<u8>) -> Result<Vec<u8>, Error> {
     let req = HyperRequest::deserialize(req)?;
     let (parts, body) = client.request(req).await?.into_parts();
 
@@ -48,8 +50,7 @@ async fn send_request(
     let body = body::to_bytes(body).await?;
 
     let resp = (parts, body).serialize();
-    resp_tx.send(resp).await.unwrap();
-    Ok(())
+    Ok(resp)
 }
 
 /// HTTP client driver entry point.
@@ -60,10 +61,12 @@ pub async fn run(mut req_rx: Receiver<Vec<u8>>, resp_tx: Sender<Vec<u8>>) {
         let client_clone = client.clone();
         let resp_tx_clone = resp_tx.clone();
         tokio::spawn(async move {
-            match send_request(client_clone, req, resp_tx_clone).await {
-                Err(_) => todo!("send error response"),
-                _ => (),
-            }
+            let resp = match send_request(client_clone, req).await {
+                Ok(resp) => resp,
+                Err(err) => err.serialize(),
+            };
+            // TODO: better error handling.
+            resp_tx_clone.send(resp).await.unwrap();
         });
     }
     println!("http client task exiting");
