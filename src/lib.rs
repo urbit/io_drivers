58978b2e592fe986ad6b0eee1c3f1fb336fd5530
Peter McEvoy <peter@tlon.io>
2022-06-13 18:24:59 -0700

    io: generalize request ingestion source

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,66 @@
+use std::{
+    io::{self, ErrorKind, Read},
+    mem::size_of,
+};
 use tokio::{
-    runtime,
+    self, runtime,
     sync::mpsc::{self, Receiver, Sender},
 };
 
-/// Last line of a Message.
-const MESSAGE_DELIM: &'static str = "==\n";
+/// IO request.
+mod request {
+    /// Tag identifying the type of a serialized IO request.
+    pub mod tag {
+        pub const HTTP_CLIENT: u8 = 0;
+    }
 
-/// Either an IO request or an IO response.
-type Message = Vec<String>;
+    /// Marker trait to identify the implementing type as an IO request.
+    pub trait Request: TryFrom<Vec<u8>> {}
+}
 
-/// Read incoming IO requests from stdin.
-fn read_stdin(req_tx: Sender<Message>) {
-    let stdin = std::io::stdin();
-    let mut should_exit = false;
+/// Synchronously read incoming IO requests from some input source.
+fn read_requests(mut reader: impl Read, req_tx: Sender<Vec<u8>>) {
     loop {
-        let mut req = Vec::new();
-        loop {
-            let mut line = String::new();
-            match stdin.read_line(&mut line) {
-                Err(_) | Ok(0) => {
-                    should_exit = true;
-                    break;
-                }
-                Ok(_) => {
-                    if line == MESSAGE_DELIM {
-                        break;
-                    } else {
-                        req.push(line);
-                    }
+        let req_len = {
+            const SIZE: usize = size_of::<u64>();
+            let mut len_buf = [0; SIZE];
+            if let Err(err) = reader.read_exact(&mut len_buf) {
+                match err.kind() {
+                    ErrorKind::UnexpectedEof => break,
+                    _ => todo!("handle error"),
                 }
             }
+            // TODO: make explicit that the length is big endian (network byte order)
+            u64::from_be_bytes(len_buf)
+        };
+        let mut req = Vec::with_capacity(req_len as usize);
+        match reader.read_to_end(&mut req) {
+            Ok(0) => break,
+            Ok(_) => {
+                req_tx.blocking_send(req).unwrap();
+            }
+            Err(_) => todo!("handle error"),
         }
-        if should_exit {
-            break;
-        }
-        req_tx.blocking_send(req).unwrap();
     }
     println!("stdin: exiting");
 }
 
 /// Schedule IO requests received from the stdin thread.
-async fn schedule_requests(mut req_rx: Receiver<Message>) {
+async fn schedule_requests(mut req_rx: Receiver<Vec<u8>>) {
     while let Some(req) = req_rx.recv().await {
-        for line in req {
-            print!("io: {}", line);
-        }
+        println!("io: req={}", String::from_utf8(req).unwrap());
     }
 }
 
 /// Library entry point.
 pub fn run() {
     // TODO: decide if there's a better upper bound for number of unscheduled requests.
-    let (req_tx, req_rx): (Sender<Message>, Receiver<Message>) = mpsc::channel(1024);
+    let (req_tx, req_rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel(1024);
 
     // Read requests in a dedicated thread because tokio doesn't seem to implement async reads from
     // stdin.
-    let stdin_thr = std::thread::spawn(move || {
-        read_stdin(req_tx);
+    let input_thr = std::thread::spawn(move || {
+        read_requests(io::stdin().lock(), req_tx);
     });
 
     runtime::Builder::new_current_thread()
@@ -64,8 +68,48 @@ pub fn run() {
         .build()
         .unwrap()
         .block_on(schedule_requests(req_rx));
-    // TODO: decide Runtime::shutdown_timeout() should be used.
+    // TODO: decide if Runtime::shutdown_timeout() should be used.
 
-    stdin_thr.join().unwrap();
+    input_thr.join().unwrap();
     println!("main: exiting");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::BufReader;
+
+    #[test]
+    fn send_request_to_stdin() {
+        let (req_tx, mut req_rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel(8);
+
+        const REQ: [u8; 13] = [
+            // Length of payload. Big endian.
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            5,
+            // Payload.
+            b'h',
+            b'e',
+            b'l',
+            b'l',
+            b'o',
+        ];
+        let reader = BufReader::new(&REQ[..]);
+        let input_thr = std::thread::spawn(move || {
+            read_requests(reader, req_tx);
+        });
+
+        let req = req_rx.blocking_recv().unwrap();
+        let req = String::from_utf8(req).unwrap();
+
+        assert_eq!(req, "hello");
+
+        input_thr.join().unwrap();
+    }
 }
