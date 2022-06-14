147b4729c465710afc4943f85f84269e811226cb
Peter McEvoy <peter@tlon.io>
2022-06-14 16:43:01 -0700

    http-client: improve error handling

--- src/http/client.rs
+++ src/http/client.rs
@@ -1,17 +1,17 @@
 use crate::{Request, Response};
 use hyper::{
     body::{self, Bytes},
-    client::Client,
+    client::{Client, HttpConnector},
     http::response::Parts,
-    Body, Request as HyperRequest,
+    Body, Error as HyperError, Request as HyperRequest,
 };
 use tokio::sync::mpsc::{Receiver, Sender};
 
 impl Request for HyperRequest<Body> {
-    type Error = ();
+    type Error = Error;
 
     fn deserialize(_req: Vec<u8>) -> Result<Self, Self::Error> {
-        todo!()
+        Err(Self::Error::Deserialization)
     }
 }
 
@@ -23,31 +23,48 @@ impl Response for HyperResponse {
     }
 }
 
+/// HTTP client error.
+pub enum Error {
+    Deserialization,
+    Hyper(HyperError),
+}
+
+impl From<HyperError> for Error {
+    fn from(err: HyperError) -> Self {
+        Self::Hyper(err)
+    }
+}
+
+/// Send an HTTP request and receive its response.
+async fn send_request(
+    client: Client<HttpConnector>,
+    req: Vec<u8>,
+    resp_tx: Sender<Vec<u8>>,
+) -> Result<(), Error> {
+    let req = HyperRequest::deserialize(req)?;
+    let (parts, body) = client.request(req).await?.into_parts();
+
+    // Wait for the entire response body to come in.
+    let body = body::to_bytes(body).await?;
+
+    let resp = (parts, body).serialize();
+    resp_tx.send(resp).await.unwrap();
+    Ok(())
+}
+
+/// HTTP client driver entry point.
 pub async fn run(mut req_rx: Receiver<Vec<u8>>, resp_tx: Sender<Vec<u8>>) {
     let client = Client::new();
 
     while let Some(req) = req_rx.recv().await {
-        let req = HyperRequest::deserialize(req);
-        if let Err(_) = req {
-            todo!("handle parse error");
-        }
-        let req = req.unwrap();
-
-        let resp = client.request(req).await;
-        if let Err(_) = resp {
-            todo!("handle request error");
-        }
-        let (parts, body) = resp.unwrap().into_parts();
-
-        // Wait for the entire response body to come in.
-        let body = body::to_bytes(body).await;
-        if let Err(_) = body {
-            todo!("handle body error");
-        }
-        let body = body.unwrap();
-
-        let resp = (parts, body).serialize();
-        resp_tx.send(resp).await.unwrap();
+        let client_clone = client.clone();
+        let resp_tx_clone = resp_tx.clone();
+        tokio::spawn(async move {
+            match send_request(client_clone, req, resp_tx_clone).await {
+                Err(_) => todo!("send error response"),
+                _ => (),
+            }
+        });
     }
     println!("http client task exiting");
 }
