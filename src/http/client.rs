d180d027ca4d892bcacb96b2bee8ab5f2decae90
Peter McEvoy <peter@tlon.io>
2022-06-15 18:38:24 -0700

    http-client: use new noun types implemented in noun crate

--- src/http/client.rs
+++ src/http/client.rs
@@ -4,33 +4,37 @@ use hyper::{
     http::response::Parts,
     Body, Error as HyperError, Request as HyperRequest,
 };
-use noun::serdes::{Cue, Jam};
+use noun::{r#enum::Noun, Cue, FromNoun, IntoNoun, Jam};
 use tokio::sync::mpsc::{Receiver, Sender};
 
 struct Request(HyperRequest<Body>);
 
-impl Cue for Request {
+impl FromNoun for Request {
     type Error = Error;
+    type Noun = Noun;
 
-    fn cue(_jammed_val: Vec<u8>) -> Result<Self, Self::Error> {
-        Err(Self::Error::Cue)
+    fn from_noun(_noun: Self::Noun) -> Result<Self, Self::Error> {
+        todo!()
     }
 }
 
 struct Response(Parts, Bytes);
 
-impl Jam for Response {
+impl IntoNoun for Response {
     type Error = Error;
+    type Noun = Noun;
 
-    fn jam(self) -> Result<Vec<u8>, Self::Error> {
-        Err(Self::Error::Jam)
+    fn into_noun(self) -> Result<Self::Noun, Self::Error> {
+        todo!()
     }
 }
 
 #[derive(Debug)]
 enum Error {
     Cue,
+    FromNoun,
     Hyper(HyperError),
+    IntoNoun,
     Jam,
 }
 
@@ -40,23 +44,32 @@ impl From<HyperError> for Error {
     }
 }
 
-impl Jam for Error {
+impl IntoNoun for Error {
     type Error = Self;
+    type Noun = Noun;
 
-    fn jam(self) -> Result<Vec<u8>, Self::Error> {
-        Err(Self::Jam)
+    fn into_noun(self) -> Result<Self::Noun, Self::Error> {
+        todo!()
     }
 }
 
 /// Send an HTTP request and receive its response.
 async fn send_request(client: Client<HttpConnector>, req: Vec<u8>) -> Result<Vec<u8>, Error> {
-    let req = Request::cue(req)?;
+    // TODO: better error handling.
+    let req_noun = Noun::cue(req).map_err(|_| Error::Cue)?;
+    // TODO: better error handling.
+    let req = Request::from_noun(req_noun).map_err(|_| Error::FromNoun)?;
     let (parts, body) = client.request(req.0).await?.into_parts();
 
     // Wait for the entire response body to come in.
     let body = body::to_bytes(body).await?;
 
-    let resp = Response(parts, body).jam()?;
+    // TODO: better error handling.
+    let resp = Response(parts, body)
+        .into_noun()
+        .map_err(|_| Error::IntoNoun)?
+        .jam()
+        .map_err(|_| Error::Jam)?;
     Ok(resp)
 }
 
@@ -70,7 +83,7 @@ pub async fn run(mut req_rx: Receiver<Vec<u8>>, resp_tx: Sender<Vec<u8>>) {
         tokio::spawn(async move {
             let resp = match send_request(client_clone, req).await {
                 Ok(resp) => resp,
-                Err(err) => err.jam().expect("failed to jam error"),
+                Err(err) => err.into_noun().expect("into noun").jam().expect("jam"),
             };
             // TODO: better error handling.
             resp_tx_clone.send(resp).await.unwrap();
