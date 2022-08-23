920d81d6b21f14a3be783ec9c820b305a1fdcc88
Peter McEvoy <git@mcevoypeter.com>
2022-08-23 17:03:04 -0600

    Replace file system driver tag with request enum

--- src/fs.rs
+++ src/fs.rs
@@ -1,4 +1,4 @@
-use crate::{Driver, Status, QUEUE_SIZE};
+use crate::{atom_as_str, Driver, Status, QUEUE_SIZE};
 use log::{debug, warn};
 use noun::{
     convert::{self, TryFromNoun},
@@ -10,54 +10,77 @@ use tokio::{
     task::JoinHandle,
 };
 
-/// Types of requests that can be handled by the filesystem driver.
-#[repr(u32)]
-enum Tag {
-    UpdateFileSystem,
-    CommitMountPoint,
-    DeleteMountPoint,
-    ListMountPoints,
+/// Requests that can be handled by the filesystem driver.
+enum Request {
+    UpdateFileSystem(UpdateFileSystem),
+    CommitMountPoint(CommitMountPoint),
+    DeleteMountPoint(DeleteMountPoint),
+    ListMountPoints(ListMountPoints),
 }
 
-impl TryFromNoun<&Noun> for Tag {
-    fn try_from_noun(noun: &Noun) -> Result<Self, convert::Error> {
-        if let Noun::Atom(atom) = noun {
-            if let Ok(atom) = atom.as_str() {
-                // These tag names are terrible, but we unfortunately can't do anything about it
-                // here because they're determined by the kernel.
-                match atom {
-                    "ergo" => Ok(Self::UpdateFileSystem),
-                    "dirk" => Ok(Self::CommitMountPoint),
-                    "ogre" => Ok(Self::DeleteMountPoint),
-                    "hill" => Ok(Self::ListMountPoints),
-                    _ => Err(convert::Error::ImplType),
+impl TryFromNoun<Noun> for Request {
+    fn try_from_noun(req: Noun) -> Result<Self, convert::Error> {
+        if let Noun::Cell(req) = req {
+            if let Noun::Atom(tag) = &*req.head() {
+                // These tag names are terrible, but we unfortunatley can't do anything about
+                // it here because they're determined by the kernel.
+                match atom_as_str(tag)? {
+                    // Update the file system.
+                    "ergo" => {
+                        if let Noun::Cell(data) = &*req.tail() {
+                            if let Noun::Atom(mount_point) = &*data.head() {
+                                Ok(Self::UpdateFileSystem(UpdateFileSystem {
+                                    mount_point: atom_as_str(mount_point)?.to_string(),
+                                }))
+                            } else {
+                                Err(convert::Error::UnexpectedCell)
+                            }
+                        } else {
+                            Err(convert::Error::UnexpectedAtom)
+                        }
+                    }
+                    _ => todo!(),
                 }
             } else {
-                Err(convert::Error::AtomToStr)
+                Err(convert::Error::UnexpectedCell)
             }
         } else {
-            Err(convert::Error::UnexpectedCell)
+            Err(convert::Error::UnexpectedAtom)
         }
     }
 }
+
+/// A request to update the file system.
+struct UpdateFileSystem {
+    mount_point: String,
+}
+
+/// A request to commit a mount point.
+struct CommitMountPoint {}
+
+/// A request to delete a mount point.
+struct DeleteMountPoint {}
+
+/// A request to list the mount points.
+struct ListMountPoints {}
 
 /// The filesystem driver.
 pub struct FileSystem {}
 
 impl FileSystem {
-    fn update_file_system(&self) {
+    fn update_file_system(&self, req: UpdateFileSystem) {
         todo!()
     }
 
-    fn commit_mount_point(&self) {
+    fn commit_mount_point(&self, req: CommitMountPoint) {
         todo!()
     }
 
-    fn delete_mount_point(&self) {
+    fn delete_mount_point(&self, req: DeleteMountPoint) {
         todo!()
     }
 
-    fn list_mount_points(&self) {
+    fn list_mount_points(&self, req: ListMountPoints) {
         todo!()
     }
 }
@@ -81,25 +104,12 @@ macro_rules! impl_driver {
             ) -> JoinHandle<Status> {
                 let task = tokio::spawn(async move {
                     while let Some(req) = input_rx.recv().await {
-                        if let Noun::Cell(req) = req {
-                            let (tag, req) = req.into_parts();
-                            match Tag::try_from_noun(&*tag) {
-                                Ok(Tag::UpdateFileSystem) => self.update_file_system(),
-                                Ok(Tag::CommitMountPoint) => self.commit_mount_point(),
-                                Ok(Tag::DeleteMountPoint) => self.delete_mount_point(),
-                                Ok(Tag::ListMountPoints) => self.list_mount_points(),
-                                _ => {
-                                    warn!(
-                                        target: Self::name(),
-                                        "ignoring request with unknown tag %{}", tag
-                                    );
-                                }
-                            }
-                        } else {
-                            warn!(
-                                target: Self::name(),
-                                "ignoring request because it's an atom"
-                            );
+                        match Request::try_from_noun(req) {
+                            Ok(Request::UpdateFileSystem(req)) => self.update_file_system(req),
+                            Ok(Request::CommitMountPoint(req)) => self.commit_mount_point(req),
+                            Ok(Request::DeleteMountPoint(req)) => self.delete_mount_point(req),
+                            Ok(Request::ListMountPoints(req)) => self.list_mount_points(req),
+                            _ => todo!(),
                         }
                     }
                     Status::Success
