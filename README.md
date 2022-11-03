bc5ffa2c92370dfcffbffc51c45b0e354db57c62
Peter McEvoy <git@mcevoypeter.com>
2022-11-03 16:08:12 -0400

    Improve status of logging in integration tests

--- README.md
+++ README.md
@@ -28,6 +28,17 @@ To build and run the test suite, run:
 $ cargo test
 ```
 
+The test suite includes integration tests, which execute the binary defined by
+`src/main.rs` in a subprocess. Logging output from the binary when executed as a
+subprocess can be captured by defining the `URBIT_IO_DRIVERS_LOG` environment
+variable. For example:
+```console
+$ URBIT_IO_DRIVERS_LOG=test.log cargo test
+```
+Note that each `#[test]` test defined in files in `tests/` will spawn a unique
+version of the binary, which means that logging output will likely be
+interleaved.
+
 To build and view the documentation, run:
 ```console
 $ cargo doc --open
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ use io_drivers::{http::client::http_client_run, Status};
 use simplelog::{Config, LevelFilter, WriteLogger};
 use std::{env, fs::File};
 
-/// File to write all logging statements to.
-const LOG_FILE: &'static str = "io_drivers.log";
-
 fn main() -> Status {
     let mut args = env::args();
     if args.len() != 2 {
@@ -12,12 +9,18 @@ fn main() -> Status {
     }
 
     let driver = args.nth(1).unwrap_or(String::from("unknown"));
-    WriteLogger::init(
-        LevelFilter::Debug,
-        Config::default(),
-        File::create(LOG_FILE).expect("create log file"),
-    )
-    .expect("initialize logger");
+    if let Ok(log) = env::var("URBIT_IO_DRIVERS_LOG") {
+        WriteLogger::init(
+            LevelFilter::Debug,
+            Config::default(),
+            File::options()
+                .create(true)
+                .append(true)
+                .open(log)
+                .expect("create log file"),
+        )
+        .expect("initialize logger");
+    }
     match &driver[..] {
         "http-client" => http_client_run(),
         _ => Status::NoDriver,
